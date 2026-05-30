b4253c85aada36704de029c0962b73b27427dcf6
donaldsam001 <nongvansam07072005@gmail.com>
2026-05-30 09:53:09 +0700

    error tailwindcss

--- src/app/task-new/task-new.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-task-new',
-  imports: [],
-  templateUrl: './task-new.html',
-  styleUrl: './task-new.css',
-})
-export class TaskNew {}
